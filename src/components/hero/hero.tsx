42faa057f2e3078616d26631255e4639c18a138f
Kevin Wu <kevinwu098@gmail.com>
2023-10-28 04:11:50 -0700

    Add skeleton of user stats

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,7 +3,7 @@ import Stats from "./stats/stats";
 
 const Hero = () => {
   return (
-    <div className="flex h-[calc(100vh-4rem)] w-[100%] place-content-center justify-between px-64">
+    <div className="flex h-[calc(100vh-6rem)] w-[100%] items-center justify-between px-32">
       <Stats />
       <div>HERO</div>
     </div>
--- src/components/hero/stats/stats.tsx
+++ src/components/hero/stats/stats.tsx
@@ -1,10 +1,109 @@
+import Image from "next/image";
 import React from "react";
+import eatr from "../../../public/images/eatr.png";
+
+const Genders = ["Male", "Female", "Non-Binary"];
+const DietaryRestrictions = ["None", "Vegetarian", "Vegan"];
+const DietaryGoals = ["Gain Weight", "Maintain Weight", "Lose Weight"];
+
+export const ArrowIcon = () => {
+  return (
+    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+      <svg
+        className="h-4 w-4 fill-current"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+      </svg>
+    </div>
+  );
+};
 
 const Stats = () => {
   return (
-    <div>
-      <div>STATS</div>
-    </div>
+    <>
+      <div className="= flex h-[40rem] w-96 flex-col gap-10 rounded-xl bg-neutral-300 p-4 text-primary drop-shadow-lg">
+        <div className="flex place-content-center text-2xl font-medium">
+          Your Zot-Stats!
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <div>
+            <label className="mb-1 block text-xs font-bold uppercase tracking-wide text-gray-700">
+              Height {"(inches)"}
+            </label>
+            <input
+              className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 drop-shadow-sm focus:border-gray-500 focus:bg-white focus:outline-none"
+              type="text"
+              placeholder="60"
+            />
+          </div>
+
+          <div>
+            <label className="mb-1 block text-xs font-bold uppercase tracking-wide text-gray-700">
+              Weight {"(lbs)"}
+            </label>
+            <input
+              className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 drop-shadow-sm focus:border-gray-500 focus:bg-white focus:outline-none"
+              type="text"
+              placeholder="100"
+            />
+          </div>
+
+          <div>
+            <label className="mb-1 block text-xs font-bold uppercase tracking-wide text-gray-700">
+              Gender
+            </label>
+            <div className="relative">
+              <select className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 drop-shadow-sm focus:border-gray-500 focus:bg-white focus:outline-none">
+                {Genders.map((gender) => (
+                  <option key={gender}>{gender}</option>
+                ))}
+              </select>
+              <ArrowIcon />
+            </div>
+          </div>
+
+          <div>
+            <label className="mb-1 block text-xs font-bold uppercase tracking-wide text-gray-700">
+              Dietary Restriction
+            </label>
+            <div className="relative">
+              <select className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 drop-shadow-sm focus:border-gray-500 focus:bg-white focus:outline-none">
+                {DietaryRestrictions.map((restriction) => (
+                  <option key={restriction}>{restriction}</option>
+                ))}
+              </select>
+              <ArrowIcon />
+            </div>
+          </div>
+
+          <div>
+            <label className="mb-1 block text-xs font-bold uppercase tracking-wide text-gray-700">
+              Dietary Goal
+            </label>
+            <div className="relative">
+              <select className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 drop-shadow-sm focus:border-gray-500 focus:bg-white focus:outline-none">
+                {DietaryGoals.map((goal) => (
+                  <option key={goal}>{goal}</option>
+                ))}
+              </select>
+              <ArrowIcon />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex place-content-center">
+          <Image
+            src={eatr}
+            alt={"Anteater with food"}
+            width={100}
+            height={100}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
