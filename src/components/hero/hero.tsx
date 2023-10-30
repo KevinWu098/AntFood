50110a4fa81d71be4b2a2f674c0c7b6058283c78
Kevin Wu <kevinwu098@gmail.com>
2023-10-30 01:42:24 -0700

    Begin file restructuring

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,11 +1,28 @@
-import React from "react";
-import Stats from "./stats/stats";
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import Profile from "../profile/profile";
+import Meal from "../meal/meal";
+
+import { auth } from "@/config/firebase";
+import { User } from "firebase/auth";
 
 const Hero = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <div className="flex h-[calc(100vh-6rem)] w-[100%] items-center justify-between px-32">
-      <Stats />
-      <div>HERO</div>
+    <div className="flex h-[calc(100vh-6rem)] w-[100%] justify-between px-32">
+      <Profile />
+      <Meal />
     </div>
   );
 };
