c97e84e3777ce0d9f9cbb869c3b70a2be93e7556
Kevin Wu <kevinwu098@gmail.com>
2023-10-31 12:03:56 -0700

    Update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Auth } from "./auth";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex h-16 items-center justify-between px-16 py-12 lg:px-24">
       <div className="text-3xl font-semibold tracking-wide text-primary">
-        🐜 AntFood
+        <Link href="/">🐜 AntFood</Link>
       </div>
-      <div className="">
+      <div className="flex flex-row items-center gap-6">
+        <div className="flex flex-row gap-6 text-lg text-primary">
+          <Link href="/profile">Profile</Link>
+          <Link href="/meals">Meals</Link>
+        </div>
         <Auth />
       </div>
     </div>
